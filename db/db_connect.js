41a161328e37b06d4db48789fac2eedfaa793f96
SantiagoGaviria <gaviriasantiago1997@gmail.com>
2022-10-17 20:30:48 -0500

    conexion mongo railway

--- db/db_connect.js
+++ db/db_connect.js
@@ -4,6 +4,7 @@ const db = async () => {
   const connectDB = await mongoose.connect(
     "mongodb://mongo:MEIisySKwOZnOHvf0BJL@containers-us-west-108.railway.app:7313"
   );
+
   console.log(connectDB.connection.db.databaseName);
 };
 
