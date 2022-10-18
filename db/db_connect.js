cfdde8c7fc5d17ea85a133f2b8a53c645b61a54f
SantiagoGaviria <gaviriasantiago1997@gmail.com>
2022-10-17 20:09:51 -0500

    actualizacion mongo

--- db/db_connect.js
+++ db/db_connect.js
@@ -1,10 +1,10 @@
-import mongoose from "mongoose" 
+import mongoose from "mongoose";
 
 const db = async () => {
-  const connectDB = await mongoose.connect("mongodb://localhost:27017/nodedocker")
-  console.log(connectDB.connection.db.databaseName)
-}
+  const connectDB = await mongoose.connect(
+    "mongodb://mongo:MEIisySKwOZnOHvf0BJL@containers-us-west-108.railway.app:7313"
+  );
+  console.log(connectDB.connection.db.databaseName);
+};
 
-export default db
-
-
+export default db;
