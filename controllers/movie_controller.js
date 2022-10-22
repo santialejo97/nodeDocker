8895d0e9d56fe93887e0810a74a9510b3e0b5f9f
Santiago Alejandro Gaviria Murcia <69436478+santialejo97@users.noreply.github.com>
2022-10-22 14:26:13 -0500

    endpoints de peliculas

--- controllers/movie_controller.js
+++ controllers/movie_controller.js
@@ -23,7 +23,83 @@ export const moviePopular = async (req = request, res = response) => {
     console.log(error);
     res.status(500).json({
       ok: false,
-      mfg: "validar logs",
+      msg: "validar logs",
+    });
+  }
+};
+
+export const searchMovie = async (req = request, res = response) => {
+  const pelicula = req.query.peli;
+  try {
+    const respo = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${pelicula}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TOKEN_TMDB}`,
+        },
+      }
+    );
+    const movie = await respo.json();
+    res.status(200).json({
+      ok: true,
+      movie,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "validar logs",
+    });
+  }
+};
+
+export const findMovie = async (req = request, res = response) => {
+  const pelicula = req.headers.movieid;
+
+  try {
+    const respo = await fetch(
+      `https://api.themoviedb.org/3/movie/${pelicula}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TOKEN_TMDB}`,
+        },
+      }
+    );
+    const movie = await respo.json();
+    res.status(200).json({
+      ok: true,
+      movie,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "validar logs",
+    });
+  }
+};
+
+export const getCast = async (req = request, res = response) => {
+  const pelicula = req.headers.movieid;
+  try {
+    const respo = await fetch(
+      `https://api.themoviedb.org/3/movie/${pelicula}/credits`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TOKEN_TMDB}`,
+        },
+      }
+    );
+    const cast = await respo.json();
+    res.status(200).json({
+      ok: true,
+      cast: cast.cast,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "validar logs",
     });
   }
 };
--- routes/movie_router.js
+++ routes/movie_router.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { moviePopular } from "../controllers/movie_controller.js";
+import {
+  findMovie,
+  getCast,
+  moviePopular,
+  searchMovie,
+} from "../controllers/movie_controller.js";
 
 const router = Router();
 
 router.get("/popular", moviePopular);
+router.get("/search", searchMovie);
+router.get("/findmovie", findMovie);
+router.get("/cast", getCast);
 
 export default router;
